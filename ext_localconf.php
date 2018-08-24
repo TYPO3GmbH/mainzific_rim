f6a1418979b443a2c06ddc7d5c8868cea06c9b34
Anja Leichsenring <anja.leichsenring@typo3.com>
2018-08-24 18:08:14 +0200

    [TASK] Brand the Login Screen

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,3 +5,15 @@ defined('TYPO3_MODE') || die();
  * Add default RTE configuration
  */
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['mainzific_rim'] = 'EXT:mainzific_rim/Configuration/RTE/Default.yaml';
+
+/***************
+ * Add the login screen logo
+ */
+// @extensionScannerIgnoreLine
+$backendConfiguration = $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'];
+if (!is_array($backendConfiguration)) {
+    $backendConfiguration = unserialize($backendConfiguration);
+}
+// Login Logo
+$backendConfiguration['loginLogo'] = 'EXT:mainzific_rim/Resources/Public/Images/mainzific_rim.svg';
+$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'] = serialize($backendConfiguration);
