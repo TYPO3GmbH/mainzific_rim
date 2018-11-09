022627800b04ba1bd90eada202f97703e9ec1086
Anja Leichsenring <anja.leichsenring@typo3.com>
2018-11-09 16:57:56 +0100

    [TASK] Set version to 1.0.3-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,5 +27,5 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => 1,
     'author' => 'TYPO3 GmbH',
     'author_email' => 'info@typo3.com',
-    'version' => '1.0.2',
+    'version' => '1.0.3-dev',
 ];
